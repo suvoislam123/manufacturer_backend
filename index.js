4c7d3f9656ea85c77a618b6b7aa7d2239e2df138
suvoislam123 <suvoislam753@gmail.com>
2022-05-29 13:03:16 +0600

    new changes in server file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
